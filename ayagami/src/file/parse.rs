AyagamiDev/ayagami

commit aaf0c06f99b0f52f120d65979363a481038e1761

file: Keep separate count post-validation
So we can move to unsafe array indexing later, keep a separate
raw_count and count for each object. count is only set post
validation, and View construction always validates against count.
This makes sure that we cannot accidentally create a View before
pre-validation is complete and we know all the arrays have the same
length.

Signed-off-by: Hoshino Lina <lina@lina.yt>

ayagami/src/file/parse.rs
@@ -368,64 +368,68 @@ impl ParsedModel {
         }
 
         // Internal stuff
-        self.part.i_blend_form_maps = flat_vec(self.part.count, None.into());
-        self.art_mesh.i_blend_form_maps = flat_vec(self.art_mesh.count, None.into());
-        self.rot_deformer.i_blend_form_maps = flat_vec(self.rot_deformer.count, None.into());
-        self.warp_deformer.i_blend_form_maps = flat_vec(self.warp_deformer.count, None.into());
-        self.glue.i_blend_form_maps = flat_vec(self.glue.count, None.into());
+        self.part.i_blend_form_maps = flat_vec(self.part.raw_count, None.into());
+        self.art_mesh.i_blend_form_maps = flat_vec(self.art_mesh.raw_count, None.into());
+        self.rot_deformer.i_blend_form_maps = flat_vec(self.rot_deformer.raw_count, None.into());
+        self.warp_deformer.i_blend_form_maps = flat_vec(self.warp_deformer.raw_count, None.into());
+        self.glue.i_blend_form_maps = flat_vec(self.glue.raw_count, None.into());
+        self.offscreen_part.i_blend_form_maps =
+            flat_vec(self.offscreen_part.raw_count, None.into());
 
-        self.rot_deformer.i_deformer = flat_vec(self.rot_deformer.count, None.into());
-        self.warp_deformer.i_deformer = flat_vec(self.warp_deformer.count, None.into());
+        self.rot_deformer.i_deformer = flat_vec(self.rot_deformer.raw_count, None.into());
+        self.warp_deformer.i_deformer = flat_vec(self.warp_deformer.raw_count, None.into());
 
         if ver < Version::V3_3 {
             debug!("Upgrading to V3.3");
-            self.warp_deformer.bilinear_interpolation = flat_vec(self.warp_deformer.count, false);
+            self.warp_deformer.bilinear_interpolation =
+                flat_vec(self.warp_deformer.raw_count, false);
         }
         // V4_0: Added bit in render_config, no need to upgrade
         if ver < Version::V4_2 {
             debug!("Upgrading to V4.2");
-            self.param.unk_zero_2 = flat_vec(self.param.count, Default::default());
-            self.param.i_keypoints = flat_vec(self.param.count, IKeypoint(0));
-            self.param.cnt_keypoints = flat_vec(self.param.count, 0);
-            self.param.blendshape = flat_vec(self.param.count, false);
-            self.param.i_blend_maps = flat_vec(self.param.count, IBlendParamMap(0));
-            self.param.cnt_blend_maps = flat_vec(self.param.count, 0);
+            self.param.unk_zero_2 = flat_vec(self.param.raw_count, Default::default());
+            self.param.i_keypoints = flat_vec(self.param.raw_count, IKeypoint(0));
+            self.param.cnt_keypoints = flat_vec(self.param.raw_count, 0);
+            self.param.blendshape = flat_vec(self.param.raw_count, false);
+            self.param.i_blend_maps = flat_vec(self.param.raw_count, IBlendParamMap(0));
+            self.param.cnt_blend_maps = flat_vec(self.param.raw_count, 0);
 
             debug!("Upgrading colors from < V4.2 to V5.0");
             let idx = self.default_colors(1);
-            self.art_mesh.i_color_forms = flat_vec(self.art_mesh.count, idx);
-            self.warp_deformer.i_color_forms = flat_vec(self.warp_deformer.count, idx);
-            self.rot_deformer.i_color_forms = flat_vec(self.rot_deformer.count, idx);
+            self.art_mesh.i_color_forms = flat_vec(self.art_mesh.raw_count, idx);
+            self.warp_deformer.i_color_forms = flat_vec(self.warp_deformer.raw_count, idx);
+            self.rot_deformer.i_color_forms = flat_vec(self.rot_deformer.raw_count, idx);
             self.art_mesh_form.i_multiply_color =
-                flat_vec(self.art_mesh_form.count, IMultiplyColor(idx));
+                flat_vec(self.art_mesh_form.raw_count, IMultiplyColor(idx));
             self.art_mesh_form.i_screen_color =
-                flat_vec(self.art_mesh_form.count, IScreenColor(idx));
-            self.warp_form.i_multiply_color = flat_vec(self.warp_form.count, IMultiplyColor(idx));
-            self.warp_form.i_screen_color = flat_vec(self.warp_form.count, IScreenColor(idx));
-            self.rot_form.i_multiply_color = flat_vec(self.rot_form.count, IMultiplyColor(idx));
-            self.rot_form.i_screen_color = flat_vec(self.rot_form.count, IScreenColor(idx));
+                flat_vec(self.art_mesh_form.raw_count, IScreenColor(idx));
+            self.warp_form.i_multiply_color =
+                flat_vec(self.warp_form.raw_count, IMultiplyColor(idx));
+            self.warp_form.i_screen_color = flat_vec(self.warp_form.raw_count, IScreenColor(idx));
+            self.rot_form.i_multiply_color = flat_vec(self.rot_form.raw_count, IMultiplyColor(idx));
+            self.rot_form.i_screen_color = flat_vec(self.rot_form.raw_count, IScreenColor(idx));
         } else if ver < Version::V5_0 {
             debug!("Upgrading colors from V4.2 to V5.0");
             self.art_mesh_form
                 .i_multiply_color
-                .resize(self.art_mesh_form.count, IMultiplyColor(0));
+                .resize(self.art_mesh_form.raw_count, IMultiplyColor(0));
             self.art_mesh_form
                 .i_screen_color
-                .resize(self.art_mesh_form.count, IScreenColor(0));
+                .resize(self.art_mesh_form.raw_count, IScreenColor(0));
             self.warp_form
                 .i_multiply_color
-                .resize(self.warp_form.count, IMultiplyColor(0));
+                .resize(self.warp_form.raw_count, IMultiplyColor(0));
             self.warp_form
                 .i_screen_color
-                .resize(self.warp_form.count, IScreenColor(0));
+                .resize(self.warp_form.raw_count, IScreenColor(0));
             self.rot_form
                 .i_multiply_color
-                .resize(self.rot_form.count, IMultiplyColor(0));
+                .resize(self.rot_form.raw_count, IMultiplyColor(0));
             self.rot_form
                 .i_screen_color
-                .resize(self.rot_form.count, IScreenColor(0));
+                .resize(self.rot_form.raw_count, IScreenColor(0));
 
-            for i in 0..self.art_mesh.count {
+            for i in 0..self.art_mesh.raw_count {
                 let v = ArtMeshView::get(self, IArtMesh::new(i as u32)).unwrap();
                 let r = v.range_forms();
                 for (color, j) in (*v.f_i_color_forms()..).zip(r.start.0..r.end.0) {
@@ -433,7 +437,7 @@ impl ParsedModel {
                     self.art_mesh_form.i_screen_color[j as usize] = IScreenColor(color);
                 }
             }
-            for i in 0..self.rot_deformer.count {
+            for i in 0..self.rot_deformer.raw_count {
                 let v = RotDeformerView::get(self, IRotDeformer::new(i as u32)).unwrap();
                 let r = v.range_forms();
                 for (color, j) in (*v.f_i_color_forms()..).zip(r.start.0..r.end.0) {
@@ -441,7 +445,7 @@ impl ParsedModel {
                     self.rot_form.i_screen_color[j as usize] = IScreenColor(color);
                 }
             }
-            for i in 0..self.warp_deformer.count {
+            for i in 0..self.warp_deformer.raw_count {
                 let v = WarpDeformerView::get(self, IWarpDeformer::new(i as u32)).unwrap();
                 let r = v.range_forms();
                 for (color, j) in (*v.f_i_color_forms()..).zip(r.start.0..r.end.0) {

ayagami/src/file/macros.rs
@@ -101,6 +101,7 @@ macro_rules! declare_object_fields {
         paste!{
             #[derive(Debug, Default)]
             pub(crate) struct [<$obj Fields>] {
+                pub(crate) raw_count: usize,
                 pub(crate) count: usize,
                 $($result)*
             }
@@ -125,7 +126,7 @@ macro_rules! parse_object_fields {
             &mut $self.[<cnt_ $field>],
             stringify!($obj.[<i_ $field>]),
             stringify!($obj.[<cnt_ $field>]),
-            $self.count,
+            $self.raw_count,
             $data
         )?;}
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
@@ -137,34 +138,34 @@ macro_rules! parse_object_fields {
             &mut $self.[<cnt_ $field>],
             stringify!($obj.[<i_ $field>]),
             stringify!($obj.[<cnt_ $field>]),
-            $self.count,
+            $self.raw_count,
             $data
         )?;}
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $data:ident { $field:ident : &&$type:ty $(,$($rest:tt)*)? }) => {
-        paste!{ Self::parse_ref(&mut $self.[<i_ $field>], stringify!($obj.$field), $self.count, $data)?; }
+        paste!{ Self::parse_ref(&mut $self.[<i_ $field>], stringify!($obj.$field), $self.raw_count, $data)?; }
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $data:ident { $field:ident : &$type:ty $(,$($rest:tt)*)? }) => {
-        paste!{ Self::parse_ref(&mut $self.[<i_ $field>], stringify!($obj.$field), $self.count, $data)?; }
+        paste!{ Self::parse_ref(&mut $self.[<i_ $field>], stringify!($obj.$field), $self.raw_count, $data)?; }
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $data:ident { $field:ident : Option<&&$type:ty> $(,$($rest:tt)*)? }) => {
-        paste!{ Self::parse_opt_ref(&mut $self.[<i_ $field>], stringify!($obj.$field), $self.count, $data)?; }
+        paste!{ Self::parse_opt_ref(&mut $self.[<i_ $field>], stringify!($obj.$field), $self.raw_count, $data)?; }
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $data:ident { $field:ident : $file_type:ty => $mem_type: ty $(,$($rest:tt)*)? }) => {
-        Self::parse_convert::<$file_type, $mem_type>(&mut $self.$field, stringify!($obj.$field), $self.count, $data)?;
+        Self::parse_convert::<$file_type, $mem_type>(&mut $self.$field, stringify!($obj.$field), $self.raw_count, $data)?;
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $data:ident { $field:ident : $type:ty $(,$($rest:tt)*)? }) => {
-        Self::parse_prim(&mut $self.$field, stringify!($obj.$field), $self.count, $data)?;
+        Self::parse_prim(&mut $self.$field, stringify!($obj.$field), $self.raw_count, $data)?;
         parse_object_fields!(@ $self $obj $data { $($($rest)*)? });
     };
 
@@ -185,54 +186,64 @@ macro_rules! parse_object_fields {
 
 macro_rules! validate_object_fields {
     ( @ $self:ident $obj:ident $model:ident { $field:ident : &&[$type:ty] $(,$($rest:tt)*)? }) => {
-        assert!(paste!{ $self.[<i_ $field>] }.len() == $self.count);
-        assert!(paste!{ $self.[<cnt_ $field>] }.len() == $self.count);
-        paste!{ Self::validate_arrayref(
-            &$self.[<i_ $field>],
-            &$self.[<cnt_ $field>],
-            stringify!($obj.[<$field>]),
-            $model
-        )?;}
+        paste! {
+            assert!($self.[<i_ $field>].len() == $self.raw_count, stringify!($obj.i_$field));
+            assert!($self.[<cnt_ $field>].len() == $self.raw_count, stringify!($obj.cnt_$field));
+            Self::validate_arrayref(
+                &$self.[<i_ $field>],
+                &$self.[<cnt_ $field>],
+                stringify!($obj.$field),
+                $model
+            )?;
+        }
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $model:ident { $field:ident : &[$type:ty] $(,$($rest:tt)*)? }) => {
-        assert!(paste!{ $self.[<i_ $field>] }.len() == $self.count);
-        assert!(paste!{ $self.[<cnt_ $field>] }.len() == $self.count);
-        paste!{ Self::validate_arrayref(
-            &$self.[<i_ $field>],
-            &$self.[<cnt_ $field>],
-            stringify!($obj.[<$field>]),
-            $model
-        )?;}
+        paste! {
+            assert!($self.[<i_ $field>].len() == $self.raw_count, stringify!($obj.i_$field));
+            assert!($self.[<cnt_ $field>].len() == $self.raw_count, stringify!($obj.cnt_$field));
+            Self::validate_arrayref(
+                &$self.[<i_ $field>],
+                &$self.[<cnt_ $field>],
+                stringify!($obj.$field),
+                $model
+            )?;
+        }
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $model:ident { $field:ident : &&$type:ty $(,$($rest:tt)*)? }) => {
-        assert!(paste!{ $self.[<i_ $field>] }.len() == $self.count);
-        paste!{ Self::validate_ref(&$self.[<i_ $field>], stringify!($obj.$field), $model)?; }
+        paste! {
+            assert!($self.[<i_ $field>].len() == $self.raw_count, stringify!($obj.i_$field));
+            Self::validate_ref(&$self.[<i_ $field>], stringify!($obj.$field), $model)?;
+        }
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $model:ident { $field:ident : &$type:ty $(,$($rest:tt)*)? }) => {
-        assert!(paste!{ $self.[<i_ $field>] }.len() == $self.count);
-        paste!{ Self::validate_ref(&$self.[<i_ $field>], stringify!($obj.$field), $model)?; }
+        paste! {
+            assert!($self.[<i_ $field>].len() == $self.raw_count, stringify!($obj.i_$field));
+            Self::validate_ref(&$self.[<i_ $field>], stringify!($obj.$field), $model)?;
+        }
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $model:ident { $field:ident : Option<&&$type:ty> $(,$($rest:tt)*)? }) => {
-        assert!(paste!{ $self.[<i_ $field>] }.len() == $self.count);
-        paste!{ Self::validate_opt_ref(&$self.[<i_ $field>], stringify!($obj.$field), $model)?; }
+        paste! {
+            assert!($self.[<i_ $field>].len() == $self.raw_count, stringify!($obj.i_$field));
+            Self::validate_opt_ref(&$self.[<i_ $field>], stringify!($obj.$field), $model)?;
+        }
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $model:ident { $field:ident : $file_type:ty => $mem_type: ty $(,$($rest:tt)*)? }) => {
-        assert!($self.$field.len() == $self.count);
+        assert!($self.$field.len() == $self.raw_count, stringify!($obj.$field));
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
     ( @ $self:ident $obj:ident $model:ident { $field:ident : $type:ty $(,$($rest:tt)*)? }) => {
-        assert!($self.$field.len() == $self.count);
+        assert!($self.$field.len() == $self.raw_count, stringify!($obj.$field));
         validate_object_fields!(@ $self $obj $model { $($($rest)*)? });
     };
 
@@ -408,7 +419,7 @@ macro_rules! declare_index_types {
                     Self(i)
                 }
                 fn bound(model: &Model) -> usize {
-                    model.[< $obj:snake >].count
+                    model.[< $obj:snake >].raw_count
                 }
             }
 
@@ -438,7 +449,7 @@ macro_rules! declare_index_types {
                     })
                 }
                 fn bound(model: &Model) -> usize {
-                    model.[< $obj:snake >].count
+                    model.[< $obj:snake >].raw_count
                 }
             }
 
@@ -537,6 +548,7 @@ macro_rules! declare_object {
                 }
 
                 fn validate(&self, model: &Model) -> Result<(), ParseError> {
+                    assert!(self.count == self.raw_count);
                     for i in 0..self.count {
                         let view = [<$obj View>]::get(model, [<I $obj>](i as u32)).unwrap();
                         view.validate()?;
@@ -544,6 +556,7 @@ macro_rules! declare_object {
                     Ok(())
                 }
                 fn post_validate(&self, model: &Model) -> Result<(), ParseError> {
+                    assert!(self.count == self.raw_count);
                     for i in 0..self.count {
                         let view = [<$obj View>]::get(model, [<I $obj>](i as u32)).unwrap();
                         view.post_validate()?;
@@ -603,7 +616,8 @@ macro_rules! declare_primitive {
 
             #[derive(Debug, Default)]
             pub(crate) struct [<$obj Fields>] {
-                pub(crate) count: usize,
+                raw_count: usize,
+                count: usize,
                 pub(crate) values: ArrayType<$mem_type>,
             }
         }
@@ -615,14 +629,14 @@ macro_rules! declare_primitive {
                 fn parse(&mut self, pass: Pass, data: &mut SectionReader) -> Result<(), ParseError> {
                     if pass as usize == Pass::$pass as usize {
                         #[allow(clippy::modulo_one)]
-                        if self.count % [<$obj:snake:upper _STRIDE>] != 0 {
+                        if self.raw_count % [<$obj:snake:upper _STRIDE>] != 0 {
                             return Err(ParseError::UnalignedItemCount(
                                 stringify!($obj),
-                                self.count,
+                                self.raw_count,
                                 [<$obj:snake:upper _STRIDE>]
                             ));
                         }
-                        let count = self.count / [<$obj:snake:upper _STRIDE>];
+                        let count = self.raw_count / [<$obj:snake:upper _STRIDE>];
                         Self::parse_prim(&mut self.values, stringify!($obj), count, data)?;
                     }
                     Ok(())
@@ -649,6 +663,7 @@ macro_rules! declare_primitive {
                 }
 
                 fn pre_validate(&self, _model: &Model) -> Result<(), ParseError> {
+                    assert!(self.values.len() * [<$obj:snake:upper _STRIDE>] == self.raw_count);
                     Ok(())
                 }
 
@@ -874,7 +889,7 @@ macro_rules! declare_file_objects {
                 pub(crate) fn load_counts(&mut self, pass: Pass, counts: &[u32]) {
                     let mut _idx = 0;
                     for_each_file_class!(pass, self, &mut obj, $spec, {
-                        obj.count = counts[_idx] as usize;
+                        obj.raw_count = counts[_idx] as usize;
                         _idx += 1;
                     });
                 }
@@ -888,6 +903,9 @@ macro_rules! declare_file_objects {
                     for_each_file_class!(Pass::Internal, self, &obj, $spec, {
                         debug!("[{0}]: Prevalidate...", std::any::type_name_of_val(obj));
                         obj.pre_validate(self)?;
+                    });
+                    for_each_file_class!(Pass::Internal, self, &mut obj, $spec, {
+                        obj.count = obj.raw_count;
                     });
                     for_each_file_class!(Pass::Internal, self, &obj, $spec, {
                         debug!("[{0}]: Validate...", std::any::type_name_of_val(obj));

commit 640ae4b10bad8def1adcacdada4f8241b484c169

file: Fix another validation regression (4.2 models)
Totally missed that the upgrade code was using views. We're intentionally
not allowing that at this point.

Fixes: aaf0c06 ("file: Keep separate count post-validation")
Fixes: #11
Signed-off-by: Hoshino Lina <lina@lina.yt>

ayagami/src/file/parse.rs
@@ -430,27 +430,27 @@ impl ParsedModel {
                 .resize(self.rot_form.raw_count, IScreenColor(0));
 
             for i in 0..self.art_mesh.raw_count {
-                let v = ArtMeshView::get(self, IArtMesh::new(i as u32)).unwrap();
-                let r = v.range_forms();
-                for (color, j) in (*v.f_i_color_forms()..).zip(r.start.0..r.end.0) {
-                    self.art_mesh_form.i_multiply_color[j as usize] = IMultiplyColor(color);
-                    self.art_mesh_form.i_screen_color[j as usize] = IScreenColor(color);
+                let form = self.art_mesh.i_forms[i].0;
+                let color = self.art_mesh.i_color_forms[i];
+                for j in 0..self.art_mesh.cnt_forms[i] {
+                    self.art_mesh_form.i_multiply_color[(form + j) as usize] = IMultiplyColor(color + j);
+                    self.art_mesh_form.i_screen_color[(form + j) as usize] = IScreenColor(color + j);
                 }
             }
             for i in 0..self.rot_deformer.raw_count {
-                let v = RotDeformerView::get(self, IRotDeformer::new(i as u32)).unwrap();
-                let r = v.range_forms();
-                for (color, j) in (*v.f_i_color_forms()..).zip(r.start.0..r.end.0) {
-                    self.rot_form.i_multiply_color[j as usize] = IMultiplyColor(color);
-                    self.rot_form.i_screen_color[j as usize] = IScreenColor(color);
+                let form = self.rot_deformer.i_forms[i].0;
+                let color = self.rot_deformer.i_color_forms[i];
+                for j in 0..self.rot_deformer.cnt_forms[i] {
+                    self.rot_form.i_multiply_color[(form + j) as usize] = IMultiplyColor(color + j);
+                    self.rot_form.i_screen_color[(form + j) as usize] = IScreenColor(color + j);
                 }
             }
             for i in 0..self.warp_deformer.raw_count {
-                let v = WarpDeformerView::get(self, IWarpDeformer::new(i as u32)).unwrap();
-                let r = v.range_forms();
-                for (color, j) in (*v.f_i_color_forms()..).zip(r.start.0..r.end.0) {
-                    self.warp_form.i_multiply_color[j as usize] = IMultiplyColor(color);
-                    self.warp_form.i_screen_color[j as usize] = IScreenColor(color);
+                let form = self.warp_deformer.i_forms[i].0;
+                let color = self.warp_deformer.i_color_forms[i];
+                for j in 0..self.warp_deformer.cnt_forms[i] {
+                    self.warp_form.i_multiply_color[(form + j) as usize] = IMultiplyColor(color + j);
+                    self.warp_form.i_screen_color[(form + j) as usize] = IScreenColor(color + j);
                 }
             }
         }